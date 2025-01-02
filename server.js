d8fecaea3c813729add05f4614f6117342cd5fa5
ahyng <ahyng1@gmail.com>
2025-01-02 20:21:23 +0900

    로그인/회원가입 기능 추가

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 
+
 const app = express();
 const port = 8080;
 const salt = 10;
 
 const User = require('./models/user');
+app.use(express.json());
 
 const dbConnect = async () => {
     try {
@@ -32,28 +34,41 @@ app.get('/', (req, res) => {
 
 // 회원가입
 app.post('/sign-up', async (req, res) => {
-    console.log(req.body);
+    console.log(await req.body);
     const hashedPwd = await bcrypt.hash(req.body.password, salt);
 
-    User.create({userId : req.body.userId, password : hashedPwd});
+    const idCheck = await User.findOne({userId : req.body.userId});
+
+    // id 중복확인
+    if (idCheck) {
+        console.log('idCheck failed');
+        res.json({idCheck : 'failed'});
+    } else {
+        User.create({userId : req.body.userId, password : hashedPwd});
+        res.json({signUp : 'succeed'});
+    }
+    
 })
 
 // 로그인
-app.post('/sign-in', (req, res) => {
+app.post('/sign-in', async (req, res) => {
     console.log(req.body);
-    const loginPwd = req.body.password;
+    const loginPwd = await req.body.password;
     
-    const user = User.findOne({userId : req.body.uwerId});
+    const user = await User.findOne({userId : req.body.userId});
 
     if (user) {
-        const checkPwd = bcrypt.compare(user.password, loginPwd);
+        const checkPwd = await bcrypt.compare(loginPwd, user.password);
         if (checkPwd) {
             console.log('succeed');
+            res.json({signIn : 'succeed'});
         } else {
             console.log('failed');
+            res.json({signIn : 'failed'});
         }
     } else {
         console.log('user not found');
+        res.json({signIn : 'user not found'});
     }
 })
 

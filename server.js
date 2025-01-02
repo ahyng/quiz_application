f5c76e41d8e7412cffdac01ba2d6e9513a2bf817
ahyng <ahyng1@gmail.com>
2025-01-02 14:04:55 +0900

    사용자 입력값 받아오기

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 const app = express();
 const port = 8080;
+const salt = 10;
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(console.log('db connected'));
+const User = require('./models/user');
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('db connected');
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+dbConnect();
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     dbConnect();
 })
-  
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+
+// 회원가입
+app.post('/sign-up', async (req, res) => {
+    console.log(req.body);
+    const hashedPwd = await bcrypt.hash(req.body.password, salt);
+
+    User.create({userId : req.body.userId, password : hashedPwd});
+})
+
+// 로그인
+app.post('/sign-in', (req, res) => {
+    console.log(req.body);
+    const loginPwd = req.body.password;
+    
+    const user = User.findOne({userId : req.body.uwerId});
+
+    if (user) {
+        const checkPwd = bcrypt.compare(user.password, loginPwd);
+        if (checkPwd) {
+            console.log('succeed');
+        } else {
+            console.log('failed');
+        }
+    } else {
+        console.log('user not found');
+    }
 })
 

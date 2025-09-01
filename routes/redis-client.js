5595a851a54f4a3d1ffa4e06080f2e2a8c005697
ahyng <ahyng1@gmail.com>
2025-09-01 16:14:39 +0900

    redis 비밀번호 설정

--- routes/redis-client.js
+++ routes/redis-client.js
@@ -1,6 +1,11 @@
 const { createClient } = require('redis');
+require('dotenv').config();
 
-const redisClient = createClient(); 
+const redisClient = createClient(
+  {
+    url: process.env.REDIS_URL
+  }
+); 
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 

cfef160e9a556940f0dcfbd17aa6e5d72f2737c4
ahyng <ahyng1@gmail.com>
2025-09-01 18:09:53 +0900

    port 변경

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));

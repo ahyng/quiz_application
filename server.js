10b61a6858734c6881e87b3239e744f91f01db49
ahyng <ahyng1@gmail.com>
2025-09-02 11:49:07 +0900

    port 부분 수정

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
